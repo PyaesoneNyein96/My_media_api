4c53ed82e6ee4d5d7ba3f45b6aac12e0e5541e1b
pyaesonenyein <pyaesonenyein.1996@gmail.com>
2023-03-23 16:04:15 +0630

    step-1 start_up

---
+++ app/Models/React.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class React extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'react_type',
+    ];
+}
